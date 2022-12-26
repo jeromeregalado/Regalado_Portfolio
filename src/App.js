d39415aa272ef848e1791c7c24256a4074889835
Jerome Regalado <jerome.regalado@softvision.com>
2022-12-26 10:26:04 +0800

    carousel added

--- src/App.js
+++ src/App.js
@@ -2,43 +2,60 @@ import logo from './logo.png';
 import './App.css';
 import React from 'react';
 import Typewriter from "typewriter-effect";
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() 
 {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Loading 
-          <Typewriter 
-          onInit=
-          {(typewriter)=> 
-            { 
-            typewriter
-            .typeString(".")
-            .pauseFor(1000)
-            .typeString(" .")
-            .pauseFor(1000)
-            .typeString(" .")
-            .deleteAll()
-            .typeString(" .")
-            .pauseFor(1000)
-            .typeString(" .")
-            .pauseFor(1000)
-            .typeString(" .")
-            .start();
-            }
-          } 
-          />
-        </p>
-          Redirecting you to Jerome's portfolio
-      </header>
-      <body class="mainpage">
+      <body className="mainpage">
+      <Carousel>
+        <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h1>Hello</h1>
+          <p>I'm Jerome </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h1>I'm a Software Engineer </h1>
+          <p>I work for Cognizant Softvision / Magenic</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h1>It's a pleasure seeing you look at my website</h1>
+          <p>
+            Hope you like my aesthetics.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+
+
         Hi, I'm Jerome. 
-        <br></br>
+        <br/>
         I am a Junior Software Engineer working at Cognizant Softvision / Magenic
-        <br></br>
+        <br/>
         It's a pleasure to see your beautiful self. I love playing apex legends. 
       </body>
     </div>
