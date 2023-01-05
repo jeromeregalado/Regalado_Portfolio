79db34ca287e8e90f257a6b35484210ea42df16e
Jerome Regalado <jerome.regalado@softvision.com>
2023-01-05 10:35:30 +0800

    fixes to cards

--- src/Components/AccordionComponents/SkillsetCards.js
+++ src/Components/AccordionComponents/SkillsetCards.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import { CircularProgressbarWithChildren } from "react-circular-progressbar";
+import { CircularProgressbarWithChildren} from "react-circular-progressbar";
 import CSSimg from '../../Images/CSSimg.png';
 import JavaImg from '../../Images/JavaImg.jpg';
 import SpringBootImg from '../../Images/SpringBootImg.png';
 import ReactImg from '../../Images/ReactImg.png';
 import CPPimg from '../../Images/CPlusPlusImg.png'
 import BootstrapImg from '../../Images/BootstrapImg.png';
+import "react-circular-progressbar/dist/styles.css";
 const current = new Date();
 const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
 
 const SkillsetCards = () =>{
     const CardInfo =[
         {image:CSSimg, title: "CSS", performance:"60"},
-        {image:JavaImg, title: "Java", performance:"60"},
-        {image:SpringBootImg, title: "Spring Boot", performance:"60"},
-        {image:CPPimg, title: "C++", performance:"60"},
+        {image:JavaImg, title: "Java", performance:"80"},
+        {image:SpringBootImg, title: "Spring Boot", performance:"70"},
+        {image:CPPimg, title: "C++", performance:"70"},
         {image:ReactImg, title: "React", performance:"60"},
         {image:BootstrapImg, title: "Bootstrap", performance:"60"}
     ]
@@ -23,12 +24,11 @@ const SkillsetCards = () =>{
     const renderCard = (card, index) => {
         return(
             <Card key={index} className='cardFormat textFormatForCard box'>
-               <Card.Img variant="top" src={card.image} className="cardImg m-auto"/>
-                    <Card.Body className="h-100">
-                    <div style={{ width: 200, height: 100,}}>
-                    <CircularProgressbarWithChildren value={card.performance} text={`${card.performance}%`}>
-                        <p className="paddingforGaugeTitle gaugeTitle">{card.title}</p>
-                    </CircularProgressbarWithChildren>
+                    <Card.Body>
+                    <div style={{padding: "20px 20px 20px 20px",maxHeight:225,maxWidth:225}}>
+                            <CircularProgressbarWithChildren value={card.performance} text={`${card.performance}%`}>
+                            <Card.Img variant="top" src={card.image} className="cardImg m-auto"/><p className="gaugeTitle paddingforGaugeTitle">{card.title}</p>
+                            </CircularProgressbarWithChildren>
                     </div>
                     </Card.Body>
             </Card>
