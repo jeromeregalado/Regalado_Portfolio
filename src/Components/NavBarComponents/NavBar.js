bcabb7ab3af917f39828322a01d4fca2272f8008
jerome.regalado <jerome.regalado@softvision.com>
2023-01-12 17:10:40 +0800

    stuff

--- src/Components/NavBarComponents/NavBar.js
+++ src/Components/NavBarComponents/NavBar.js
@@ -1,25 +1,7 @@
 import React from "react";
-import { Navbar, Offcanvas, Nav, Container, NavLink  } from "react-bootstrap";
+import { Navbar, Offcanvas, Nav, Container  } from "react-bootstrap";
 
 const NavBar = () => {
-  const sections = document.querySelectorAll("section");
-  const navLi = document.querySelectorAll("nav .container ul li");
-  window.onscroll = () => {
-    var current = "";
-  
-    sections.forEach((section) => {
-      const sectionTop = section.offsetTop;
-      if (scrollY >= sectionTop - 60) {
-        current = section.getAttribute("id"); }
-    });
-  
-    navLi.forEach((li) => {
-      NavLink.classList.remove("active");
-      if (li.classList.contains(current)) {
-        li.classList.add("active");
-      }
-    });
-  };
     return (
     <Navbar fixed="top" bg="dark" variant="dark" expand="lg">
           <Container>
@@ -37,12 +19,10 @@ const NavBar = () => {
               </Offcanvas.Header>
               <Offcanvas.Body>
                     <Nav className="justify-content-end flex-grow-1 pe-3">
-                      <ul>
-                      <li><a className="nav-links" href="#home" spy to="Home">Home</a></li>
-                      <li><a className="nav-links" href="#aboutMe" spy to="About Me">About Me</a></li>
-                      <li><a className="nav-links" href="#projects" spy to="Projects">Projects</a></li>
-                      <li><a className="nav-links" href="#contactMe" spy to="Contact Me">Contact Me</a></li>
-                      </ul>
+                      <Nav.Link className="nav-links" href="#home" spy to="Home">Home</Nav.Link>
+                      <Nav.Link className="nav-links" href="#aboutMe" spy to="About Me">About Me</Nav.Link>
+                      <Nav.Link className="nav-links" href="#projects" spy to="Projects">Projects</Nav.Link>
+                      <Nav.Link className="nav-links" href="#contactMe" spy to="Contact Me">Contact Me</Nav.Link>
                     </Nav>
               </Offcanvas.Body>
               </Navbar.Offcanvas>
