c31088dc176fc9d124c6fe9b025fbb5f868326c3
jerome.regalado <jerome.regalado@softvision.com>
2023-01-16 14:31:13 +0800

    added linkedin account

--- src/Components/NavBarComponents/FooterNavBar.js
+++ src/Components/NavBarComponents/FooterNavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Facebook, Github, Instagram, Mailbox } from 'react-bootstrap-icons';
+import { Linkedin } from 'react-bootstrap-icons/dist';
 
 const FooterNavBar = () => {
   return (
@@ -10,6 +11,7 @@ const FooterNavBar = () => {
         <a href="https://www.instagram.com/jerpyyy/" target="_blank" rel='noreferrer'><Instagram size={30} className="mx-3" /></a>
         <a href="https://github.com/jeromeregalado" target="_blank" rel='noreferrer'><Github size={30} className="mx-3" /></a>
         <a href="https://mail.google.com/mail/u/0/?fs=1&to=jerome.regalado@softvision.com&su=&body=&tf=cm" target="_blank" rel='noreferrer'><Mailbox size={30} className="mx-3" /></a>
+        <a href='https://ph.linkedin.com/in/jerome-regalado-83306b172'target="_blank" rel='noreferrer'><Linkedin size={30} className='mx-3'/></a>
         <p className="footerFormat">Jerome Regalado &#169; {new Date().getFullYear()}</p>
       </Container>
     </footer>
