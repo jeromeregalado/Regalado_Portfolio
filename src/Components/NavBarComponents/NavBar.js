6f78e9269b73f8a6edd993a7740bb2b35f9c365f
Jerome Regalado <jerome.regalado@softvision.com>
2023-01-16 21:38:41 +0800

    fixes to navbar

--- src/Components/NavBarComponents/NavBar.js
+++ src/Components/NavBarComponents/NavBar.js
@@ -4,7 +4,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 
 const NavBar = () => {
   return (
-    <Navbar fixed="top" bg="dark" variant="dark" expand="lg" id="navbar-target" collapseOnSelectgit >
+    <Navbar fixed="top" bg="dark" variant="dark" expand="lg" id="navbar-target" collapseOnSelect >
       <Container fluid>
         <Navbar.Brand href="#home">Jerome Regalado</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
