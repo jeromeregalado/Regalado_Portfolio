f823bcdcce7a2c71f9a455fb91d679900d76896b
jerome.regalado <jerome.regalado@softvision.com>
2023-01-16 16:34:18 +0800

    status fixes

--- src/Components/NavBarComponents/NavBar.js
+++ src/Components/NavBarComponents/NavBar.js
@@ -1,23 +1,19 @@
 import React, {useState} from "react";
 import { Navbar, Offcanvas, Nav, Container } from "react-bootstrap";
 
+
 const NavBar = () => {
-  const [show, setShow] = useState(false);
-
-  const toggleOffCanvas = () => {
-    setShow((show) => !show);
-  };
-
   return (
     <Navbar fixed="top" bg="dark" variant="dark" expand="lg" id="navbar-target">
       <Container fluid>
         <Navbar.Brand href="#home">Jerome Regalado</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
         <Navbar.Offcanvas
           id={`offcanvasNavbar-expand-sm`}
           aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
           placement="end"
         >
+        
           <Offcanvas.Header closeButton placeholder="start">
             <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
               Links for my webpage
@@ -28,7 +24,7 @@ const NavBar = () => {
               <Nav.Link className="nav-links" href="#home">Home</Nav.Link>
               <Nav.Link className="nav-links" href="#aboutMe">About Me</Nav.Link>
               <Nav.Link className="nav-links" href="#projects">Projects</Nav.Link>
-              <Nav.Link className="nav-links" href="#contactMe">Contact Me</Nav.Link>
+              <Nav.Link className="nav-links"  href="#contactMe">Contact Me</Nav.Link>
             </Nav>
           </Offcanvas.Body>
         </Navbar.Offcanvas>
